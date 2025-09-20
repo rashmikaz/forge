6acb7a786c2b011b41b4243fd50eacf822c6014a
rashmikaz <phrashmika@gmail.com>
2025-09-20 12:55:26 +0530

    contact form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,6 +5,68 @@ export default function ContactPage() {
       <p className="w-130 text-2xl pb-8 text-[#1E1E1E] leading-relaxed">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
       </p>
+
+      <div className="max-w-2xl mx-auto py-10 relative right-55 bottom-5">
+        <div className="border-b border-gray-300 py-8">
+          <div className="flex items-start gap-4">
+            <span className="text-sm text-gray-500">01</span>
+            <div>
+              <h3 className="text-lg  text-black">
+                Lorem ipsum dolor sit amet ?
+              </h3>
+              <p className="text-sm text-gray-500">max de</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="border-b border-gray-300 py-8">
+          <div className="flex items-start gap-4">
+            <span className="text-sm text-gray-500">02</span>
+            <div>
+              <h3 className="text-lg  text-black">
+                Lorem ipsum dolor sit amet ?
+              </h3>
+              <p className="text-sm text-gray-500">max de</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="border-b border-gray-300 py-8">
+          <div className="flex items-start gap-4">
+            <span className="text-sm text-gray-500">03</span>
+            <div>
+              <h3 className="text-lg  text-black">
+                Lorem ipsum dolor sit amet ?
+              </h3>
+              <p className="text-sm text-gray-500">max de</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="border-b border-gray-300 py-8">
+          <div className="flex items-start gap-4">
+            <span className="text-sm text-gray-500">04</span>
+            <div>
+              <h3 className="text-lg  text-black">
+                Lorem ipsum dolor sit amet ?
+              </h3>
+              <p className="text-sm text-gray-500">max de</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="border-b border-gray-300 py-8">
+          <div className="flex items-start gap-4">
+            <span className="text-sm text-gray-500">05</span>
+            <div>
+              <h3 className="text-lg  text-black">
+                Lorem ipsum dolor sit amet ?
+              </h3>
+              <p className="text-sm text-gray-500">max de</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
