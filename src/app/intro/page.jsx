81e9fb0e1f3ee63300e872a9e8eb53dec174541e
rashmikaz <phrashmika@gmail.com>
2025-09-26 08:41:33 +0530

    add animation for button

--- src/app/intro/page.jsx
+++ src/app/intro/page.jsx
@@ -1,44 +1,52 @@
-import React from "react";
-import Button from "../components/Button";
+import React, { useEffect } from "react";
+import { motion, useMotionValue, useSpring } from "framer-motion";
 
 const Intro = () => {
-  return (
-    <>
-      <div className="pt-25 pl-44 pr-44">
-        <h1 className="w-270 text-6xl leading-16">
-          <span className="ml-60">We build </span>custom SaaS, AI tools, and
-          lightning-fast MVPs for startups We build custom SaaS, AI tools, and
-          abds lightning-fast MVPs for startups We build
-        </h1>
-        <div className="ml-155 relative bottom-10">
-          <p className="w-128 text-2xl pb-8 text-[#1E1E1E] leading-relaxed">
-            We build custom SaaS, AI tools, and lightning-fast MVPs for startups
-            We build custom SaaS, AI tools, and lightning-fast MVPs for
-          </p>
-        </div>
+  const y = useMotionValue(0);
 
-        <div className="ml-195 relative bottom-10">
-          <div className="pb-10">
-            <div
-              className="
-            w-35 h-35 
-            rounded-full 
-            bg-blue-600 
-            text-white 
-            text-sm 
-            flex items-center justify-center 
-            transition-all duration-300 
-            hover:bg-[#22262C] 
-            hover:translate-y-[-10px] 
-            hover:shadow-lg
-          "
-            >
-              about us
-            </div>
-          </div>
-        </div>
+  const smoothY = useSpring(y, { stiffness: 100, damping: 25 });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+
+      y.set(-scrollTop * 0.25);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [y]);
+
+  return (
+    <div className="pt-25 pl-44 pr-44 h-[2000px]">
+      <h1 className="w-270 text-6xl leading-16">
+        <span className="ml-60">We build </span>
+        custom SaaS, AI tools, and lightning-fast MVPs for startups We build
+        custom SaaS, AI tools, and abds lightning-fast MVPs for startups We
+        build
+      </h1>
+
+      <div className="ml-155 relative bottom-12 ">
+        <p className="w-128 text-2xl pb-8 text-[#1E1E1E] leading-relaxed">
+          We build custom SaaS, AI tools, and lightning-fast MVPs for startups
+          We build custom SaaS, AI tools, and lightning-fast MVPs for
+        </p>
       </div>
-    </>
+
+      <motion.div style={{ y: smoothY }} className="ml-195">
+        <div
+          className="pb-10 bg-blue-600 
+            text-white w-40 h-40 
+            rounded-full text-sm 
+            flex items-center justify-center relative top-55"
+        >
+          About Us
+        </div>
+      </motion.div>
+    </div>
   );
 };
 
